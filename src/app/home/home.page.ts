c9455af5a4e4694df8a9629b97ca708175236ab8
Pedro S <97417230+sousa-p@users.noreply.github.com>
2023-05-04 11:57:05 +0000

    Adicionando Ts e dark theme

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,12 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  item_selecionado: string = 'ds';
+
+  selecionarCard(card: any){
+    this.item_selecionado = card.detail.value;
+  }
 }
