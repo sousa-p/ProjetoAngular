209d26df490cf562d11b215bae4d526f0e9f8eef
Pedro S <97417230+sousa-p@users.noreply.github.com>
2023-05-25 12:40:36 +0000

    Criação do projeto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -7,12 +8,23 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, RouterModule],
 })
 export class HomePage {
-  item_selecionado: string = 'ds';
+  calendario = {
+    mes: 'Maio',
+    ano: 2023,
+    foto: 'https://images.pexels.com/photos/5445574/pexels-photo-5445574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    dias_da_semana: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'],
+    primeira_semana: [30,1,2,3,4,5,6],
+    segunda_semana: [7,8,9,10,11,12,13],
+    terceira_semana: [14,15,16,17,18,19,20],
+    quarta_semana: [21,22,23,24,25,26,27],
+    quinta_semana: [28,29,30,31,1,2,3]
+  }
 
-  selecionarCard(card: any){
-    this.item_selecionado = card.detail.value;
+  darkTheme = false
+  toogleTheme () {
+    this.darkTheme = !this.darkTheme;
   }
 }
